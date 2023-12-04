7aa42f7495657b8375f94d111e0ce10c865e85d8
omkar7b <omkarbende777@gmail.com>
2023-12-04 16:14:35 +0530

    Ready to deploy

--- util/database.js
+++ util/database.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 const sequelize = new Sequelize(process.env.DATABASE_NAME, process.env.DATABASE_USERNAME, process.env.DATABASE_PASSWORD,{
     dialect: 'mysql',
-    host: 'localhost',
+    host: process.env.DATABASE_HOST,
 });
 
 module.exports = sequelize;
